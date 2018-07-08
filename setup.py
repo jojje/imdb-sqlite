ec9d2676b94efab450463bd1878e6f6c5639e6e1
Jonas Tingeborn <tinjon@gmail.com>
2018-07-08 17:38:41 +0200

    Version bump to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='imdb-sqlite',
-    version='0.1.2',
+    version='0.1.3',
     author='Jonas Tingeborn',
     author_email='tinjon+pip@gmail.com',
     description='Imports IMDB TSV files into a SQLite database',
